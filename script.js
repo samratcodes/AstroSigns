9eddc2f35a654190bd32e2b3b5782c3a83f315a5
samratcodes <samratcodess@gmail.com>
2023-08-16 19:13:10 +0545

    Feat: input

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const Submit=document.querySelector('button');
+const date = document.querySelector('input');
 const classtext = document.querySelector(".card");
 Submit.addEventListener('click',function(e){
     e.preventDefault()
+    date.value=''
 classtext.scrollIntoView({behavior:'smooth'});
 })
